bcd5ebd15d8401c0c5c1f7481cb4227e1a2a53a8
Andrey Parvatkin <wish.tlt@gmail.com>
2020-12-02 16:43:29 +0400

    add SightListScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:places/ui/sight_list_screen.dart';
 
 void main() {
   runApp(App());
@@ -13,71 +14,7 @@ class App extends StatelessWidget {
         primarySwatch: Colors.blue,
         fontFamily: 'Roboto',
       ),
-      home: MyFirstStatelessWidget(),
-    );
-  }
-}
-
-class MyFirstStatelessWidget extends StatelessWidget {
-  int buildsCount = 0;
-
-  MyFirstStatelessWidget() {
-    print('StateLess constructor call'); // вызывается постоянно
-  }
-
-  // тут нет context
-  // Type getContextRuntimeType() {
-  //   return context.runtimeType;
-  // }
-
-  @override
-  Widget build(BuildContext context) {
-    buildsCount++;
-    print(buildsCount); // всегда выводит 1 потому что виджет пересоздается и переменная инициируется заново
-
-    return Container(
-      child: Center(
-        child: Text('Hello!'),
-      ),
-    );
-  }
-}
-
-class MyFirstStatefulWidget extends StatefulWidget {
-  MyFirstStatefulWidget() {
-    print('StateFull constructor call'); // вызывается постоянно
-  }
-
-  // и тут нет context
-  // Type getContextRuntimeType() {
-  //   return context.runtimeType;
-  // }
-
-  @override
-  _MyFirstStatefulWidgetState createState() => _MyFirstStatefulWidgetState();
-}
-
-class _MyFirstStatefulWidgetState extends State<MyFirstStatefulWidget> {
-  int buildsCount = 0;
-
-  _MyFirstStatefulWidgetState() {
-    print('State constructor call'); // вызывается 1 раз
-  }
-
-  // а тут есть
-  Type getContextRuntimeType() {
-    return context.runtimeType;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    buildsCount++;
-    print(buildsCount); // выводит номер перерисовки начиная с 1, потому что стейт не пересоздается
-
-    return Container(
-      child: Center(
-        child: Text('Hello!'),
-      ),
+      home: SightListScreen(),
     );
   }
 }
