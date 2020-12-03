fa8f2e88a793b6bc363b70bc8cd2c232e14b6e9f
Andrey Parvatkin <wish.tlt@gmail.com>
2020-12-03 13:33:02 +0400

    text widget in AppBar

--- lib/ui/sight_list_screen.dart
+++ lib/ui/sight_list_screen.dart
@@ -10,6 +10,23 @@ class SightListScreen extends StatefulWidget {
 class _SightListScreenState extends State<SightListScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        toolbarHeight: 128,
+        centerTitle: false,
+        title: Text(
+          'Список\nинтересных мест',
+          maxLines: 2,
+          overflow: TextOverflow.ellipsis,
+          style: TextStyle(
+            color: Color(0xFF3B3E5B),
+            fontSize: 32,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
   }
 }
