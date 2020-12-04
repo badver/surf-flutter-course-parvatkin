a9452cb981aa4081e0af9f7f70de12e7e4c596b4
Andrey Parvatkin <wish.tlt@gmail.com>
2020-12-04 12:31:19 +0400

    add mocks

---
+++ lib/mocks.dart
@@ -0,0 +1,28 @@
+import 'package:places/domain/sight.dart';
+
+final List<Sight> mocks = [
+  const Sight(
+    nameSight: 'Airplane Boneyard',
+    lan: 32.149989,
+    lon: -110.835842,
+    url: 'https://twistedsifter.com/wp-content/uploads/2014/02/airplane-boneyard-tucson-arizona-google-earth.jpg',
+    details: 'Tucson, Arizona',
+    type: null,
+  ),
+  const Sight(
+    nameSight: 'Mysterious Desert Pattern',
+    lan: 27.380583,
+    lon: 33.631839,
+    url: 'https://twistedsifter.com/wp-content/uploads/2014/02/crop-circle-egypt-google-earth-strange.jpg',
+    details: 'Red Sea Governorate, Egypt',
+    type: null,
+  ),
+  const Sight(
+    nameSight: 'Giant Triangle',
+    lan: 33.747252,
+    lon: -112.633853,
+    url: 'https://twistedsifter.com/wp-content/uploads/2014/02/giant-triangle-google-earth.jpg',
+    details: 'Wittmann, Arizona, USA',
+    type: null,
+  ),
+];
