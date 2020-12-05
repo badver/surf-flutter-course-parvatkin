6b9e5f6a7e63433dd41e83c398cc9b9b44824a9a
Andrey Parvatkin <wish.tlt@gmail.com>
2020-12-05 14:39:00 +0400

    extract styles, colors, widgets

--- lib/ui/screen/sight_details.dart
+++ lib/ui/screen/sight_details.dart
@@ -16,120 +16,210 @@ class SightDetails extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Stack(
-              children: [
-                Image.network(
-                  sight.url,
-                  fit: BoxFit.cover,
-                  height: 360,
-                ),
-                const Positioned(
-                  top: 36,
-                  left: 16,
-                  child: BackButton(),
-                ),
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 16, right: 16, top: 24),
-              child: Column(
-                children: [
-                  ConstrainedBox(
-                    constraints: const BoxConstraints(
-                      minWidth: double.infinity,
-                    ),
-                    child: Text(
-                      sight.nameSight,
-                      style: const TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                        color: AppColors.black,
-                      ),
-                    ),
-                  ),
-                  Row(
-                    children: [
-                      Text(
-                        sight.type,
-                        style: const TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.bold,
-                          color: AppColors.black,
-                        ),
-                      ),
-                      const SizedBox(width: 16),
-                      const WorkTime(workTime: 'закрыто до 09:00'),
-                    ],
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 24),
-                    child: Text(
-                      sight.details,
-                      style: const TextStyle(
-                        fontSize: 14,
-                        color: AppColors.black,
-                      ),
-                    ),
-                  ),
-                  FlatButton.icon(
-                    onPressed: () {},
-                    color: AppColors.green,
-                    height: 48,
-                    minWidth: double.infinity,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    icon: const Icon(
-                      Icons.input,
-                      color: Colors.white,
-                    ),
-                    label: const Text(
-                      'ПОСТРОИТЬ МАРШРУТ',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 14,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 24),
-                  const Divider(),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      FlatButton.icon(
-                        onPressed: () {},
-                        icon: const Icon(
-                          Icons.calendar_today,
-                          color: Color(0xAA7C7E92),
-                        ),
-                        label: const Text(
-                          'Запланировать',
-                          style: TextStyle(
-                            color: Color(0xAA7C7E92),
-                            fontSize: 14,
-                          ),
-                        ),
-                      ),
-                      FlatButton.icon(
-                        onPressed: () {},
-                        icon: const Icon(
-                          Icons.details,
-                          color: AppColors.black,
-                        ),
-                        label: const Text('В Избранное',
-                            style: TextStyle(
-                              color: AppColors.black,
-                              fontSize: 14,
-                            )),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
+            DetailsHeader(sight: sight),
+            DetailsBody(sight: sight),
           ],
         ),
       ),
+    );
+  }
+}
+
+class DetailsBody extends StatelessWidget {
+  const DetailsBody({
+    Key key,
+    @required this.sight,
+  }) : super(key: key);
+
+  final Sight sight;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(
+        left: 16,
+        right: 16,
+        top: 24,
+      ),
+      child: Column(
+        children: [
+          SightName(sight: sight),
+          Row(
+            children: [
+              SightType(sight: sight),
+              const SizedBox(width: 16),
+              // TODO workTime пока заглушка, видимо в будущем будет браться откуда-то динамически
+              const WorkTime(workTime: 'закрыто до 09:00'),
+            ],
+          ),
+          SightDetailsWidget(sight: sight),
+          const BuildRouteButton(),
+          const SizedBox(height: 24),
+          const Divider(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: const [
+              PlanButton(),
+              FavoritesButton(),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SightDetailsWidget extends StatelessWidget {
+  const SightDetailsWidget({
+    Key key,
+    @required this.sight,
+  }) : super(key: key);
+
+  final Sight sight;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 24),
+      child: Text(
+        sight.details,
+        style: AppTextStyles.sightDetailsDetails,
+      ),
+    );
+  }
+}
+
+class SightType extends StatelessWidget {
+  const SightType({
+    Key key,
+    @required this.sight,
+  }) : super(key: key);
+
+  final Sight sight;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      sight.type,
+      style: AppTextStyles.sightDetailsType,
+    );
+  }
+}
+
+class SightName extends StatelessWidget {
+  const SightName({
+    Key key,
+    @required this.sight,
+  }) : super(key: key);
+
+  final Sight sight;
+
+  @override
+  Widget build(BuildContext context) {
+    return ConstrainedBox(
+      constraints: const BoxConstraints(
+        minWidth: double.infinity,
+      ),
+      child: Text(
+        sight.nameSight,
+        style: AppTextStyles.sightDetailsName,
+      ),
+    );
+  }
+}
+
+class PlanButton extends StatelessWidget {
+  const PlanButton({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton.icon(
+      onPressed: () {},
+      icon: const Icon(
+        Icons.calendar_today,
+        color: AppColors.disabled,
+      ),
+      label: const Text(
+        AppTexts.toPlan,
+        style: AppTextStyles.toPlan,
+      ),
+    );
+  }
+}
+
+class FavoritesButton extends StatelessWidget {
+  const FavoritesButton({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton.icon(
+      onPressed: () {},
+      icon: const Icon(
+        Icons.details,
+        color: AppColors.black,
+      ),
+      label: const Text(
+        AppTexts.toFavorites,
+        style: AppTextStyles.toFavorites,
+      ),
+    );
+  }
+}
+
+class BuildRouteButton extends StatelessWidget {
+  const BuildRouteButton({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton.icon(
+      onPressed: () {},
+      color: AppColors.green,
+      height: 48,
+      minWidth: double.infinity,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12),
+      ),
+      icon: const Icon(
+        Icons.input,
+        color: AppColors.white,
+      ),
+      label: const Text(
+        AppTexts.buildRoute,
+        style: AppTextStyles.buildRouteButton,
+      ),
+    );
+  }
+}
+
+class DetailsHeader extends StatelessWidget {
+  const DetailsHeader({
+    Key key,
+    @required this.sight,
+  }) : super(key: key);
+
+  final Sight sight;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Image.network(
+          sight.url,
+          fit: BoxFit.cover,
+          height: 360,
+        ),
+        const Positioned(
+          top: 36,
+          left: 16,
+          child: BackButton(),
+        ),
+      ],
     );
   }
 }
@@ -146,10 +236,7 @@ class WorkTime extends StatelessWidget {
   Widget build(BuildContext context) {
     return Text(
       workTime,
-      style: const TextStyle(
-        fontSize: 14,
-        color: Color(0xFF7C7E92),
-      ),
+      style: AppTextStyles.workTime,
     );
   }
 }
@@ -167,7 +254,7 @@ class BackButton extends StatelessWidget {
       child: FlatButton(
         padding: const EdgeInsets.all(0),
         onPressed: () {},
-        color: Colors.white,
+        color: AppColors.white,
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(10),
         ),
