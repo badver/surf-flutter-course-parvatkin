83983c5a3b28edc0ee5957b1a36e15d88e53081b
Andrey Parvatkin <wish.tlt@gmail.com>
2021-02-14 17:38:53 +0400

    impl FiltersScreenButton

--- lib/ui/screen/filters_screen/filters_screen_button.dart
+++ lib/ui/screen/filters_screen/filters_screen_button.dart
@@ -1,14 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:places/ui/constants.dart';
 
 class FiltersScreenButton extends StatelessWidget {
+  final int count;
+
   const FiltersScreenButton({
     Key key,
+    this.count = 0,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder(
-      fallbackHeight: 48,
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        vertical: 8,
+        horizontal: 16,
+      ),
+      child: FlatButton(
+        onPressed: () {
+          debugPrint('Show all pressed.');
+        },
+        color: AppColorsLight.green,
+        height: 48,
+        minWidth: double.infinity,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Text(
+          '${AppTexts.show} ($count)'.toUpperCase(),
+          style: Theme.of(context).textTheme.button.copyWith(
+                color: AppColorsLight.white,
+              ),
+        ),
+      ),
     );
   }
 }
