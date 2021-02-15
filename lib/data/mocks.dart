4b2a5db3c584b86ac328e9c3d99ad3dec4a38aec
Andrey Parvatkin <wish.tlt@gmail.com>
2021-02-15 21:51:42 +0400

    more mocks

--- lib/data/mocks.dart
+++ lib/data/mocks.dart
@@ -9,7 +9,7 @@ final List<Sight> mocks = [
         'https://images.pexels.com/photos/941861/pexels-photo-941861.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     details:
         'Пряный вкус радостной жизни вместе с шеф-поваром Изо Дзандзава, благодаря которой у гостей ресторана есть возможность выбирать из двух направлений: европейского и восточного',
-    type: 'ресторан',
+    type: SightType.restaurant,
     workTime: 'закрыто до 20:00',
   ),
   const Sight(
@@ -18,7 +18,7 @@ final List<Sight> mocks = [
     lon: -110.835842,
     url: 'https://twistedsifter.com/wp-content/uploads/2014/02/airplane-boneyard-tucson-arizona-google-earth.jpg',
     details: 'Tucson, Arizona',
-    type: 'landmark',
+    type: SightType.particularPlace,
     workTime: 'закрыто до 10:00',
   ),
   const Sight(
@@ -27,7 +27,7 @@ final List<Sight> mocks = [
     lon: 33.631839,
     url: 'https://twistedsifter.com/wp-content/uploads/2014/02/crop-circle-egypt-google-earth-strange.jpg',
     details: 'Red Sea Governorate, Egypt',
-    type: 'landmark',
+    type: SightType.park,
     workTime: 'закрыто до 20:00',
   ),
   const Sight(
@@ -36,7 +36,25 @@ final List<Sight> mocks = [
     lon: -112.633853,
     url: 'https://twistedsifter.com/wp-content/uploads/2014/02/giant-triangle-google-earth.jpg',
     details: 'Wittmann, Arizona, USA',
-    type: 'landmark',
+    type: SightType.museum,
     workTime: 'c 10:00 до 20:00',
+  ),
+  const Sight(
+    nameSight: 'Giant Turtle',
+    lan: 34.747252,
+    lon: -111.633853,
+    url: 'https://c402277.ssl.cf1.rackcdn.com/photos/18421/images/hero_small/Medium_WW288394.jpg?1578408451',
+    details: 'Somewhere',
+    type: SightType.hotel,
+    workTime: 'c 7:00 до 20:00',
+  ),
+  const Sight(
+    nameSight: 'The Giant',
+    lan: 0.747252,
+    lon: 0.633853,
+    url: 'https://i.pinimg.com/originals/9f/15/de/9f15dee844b7018f63c06c321e197d1d.jpg',
+    details: 'Chiangmai Thailand',
+    type: SightType.cafe,
+    workTime: 'c 7:00 до 20:00',
   ),
 ];
