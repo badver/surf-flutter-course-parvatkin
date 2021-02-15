bcdb0d244e11757d274dc905a7b1e54fedf100d3
Andrey Parvatkin <wish.tlt@gmail.com>
2021-02-15 21:13:26 +0400

    fix slider

--- lib/ui/screen/filters_screen/filters_screen_slider.dart
+++ lib/ui/screen/filters_screen/filters_screen_slider.dart
@@ -11,38 +11,58 @@ class FiltersScreenSlider extends StatefulWidget {
 }
 
 class _FiltersScreenSliderState extends State<FiltersScreenSlider> {
-  RangeValues _rangeValues = const RangeValues(100, 10000);
+  RangeValues _rangeValues = const RangeValues(1, 100);
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            const Text(AppTexts.distance),
-            Text(
-                '${AppTexts.rangeFrom} ${_distance(_rangeValues.start)} ${AppTexts.rangeTo} ${_distance(_rangeValues.end)}'),
-          ],
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                AppTexts.distance,
+                style: Theme.of(context).textTheme.subtitle2.copyWith(
+                      fontWeight: FontWeight.normal,
+                    ),
+              ),
+              Text(
+                '${AppTexts.rangeFrom} ${_distance(_rangeValues.start)} ${AppTexts.rangeTo} ${_distance(_rangeValues.end)}',
+                style: Theme.of(context).textTheme.subtitle2.copyWith(
+                      fontWeight: FontWeight.normal,
+                      color: Theme.of(context).secondaryHeaderColor,
+                    ),
+              ),
+            ],
+          ),
         ),
         SliderTheme(
           data: SliderThemeData(
             activeTrackColor: AppColorsLight.green,
             inactiveTrackColor: Theme.of(context).disabledColor,
             trackHeight: 2,
+            rangeTrackShape: const RectangularRangeSliderTrackShape(),
+            rangeThumbShape: const RoundRangeSliderThumbShape(
+              enabledThumbRadius: 8,
+              elevation: 2,
+            ),
             thumbColor: Theme.of(context).cardColor,
           ),
           child: RangeSlider(
-            min: 100,
-            max: 10000,
+            min: 1,
+            max: 100,
             divisions: 100,
             onChanged: (RangeValues values) {
-              setState(() {
-                _rangeValues = RangeValues(
-                  _roundDistance(values.start),
-                  _roundDistance(values.end),
-                );
-              });
+              setState(
+                () {
+                  _rangeValues = RangeValues(
+                    _roundDistance(values.start),
+                    _roundDistance(values.end),
+                  );
+                },
+              );
             },
             values: _rangeValues,
           ),
@@ -52,10 +72,13 @@ class _FiltersScreenSliderState extends State<FiltersScreenSlider> {
   }
 
   String _distance(double dist) {
-    return dist < 1000 ? '${_roundDistance(dist)} ${AppTexts.meters}' : '${dist / 1000} ${AppTexts.kilometers}';
+    return dist <= 10 ? '${dist.round()}00 ${AppTexts.meters}' : '${(dist / 10).round()} ${AppTexts.kilometers}';
   }
 
   double _roundDistance(double dist) {
-    return (dist / 100).roundToDouble() * 100;
+    if (dist > 9) {
+      return (dist / 10).round() * 10.0;
+    }
+    return dist;
   }
 }
