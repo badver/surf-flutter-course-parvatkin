63262b7644acc0717f9399e538dcfac972fbb25e
Andrey Parvatkin <wish.tlt@gmail.com>
2021-02-15 21:14:27 +0400

    fix arePointsNear function

--- lib/domain/utils.dart
+++ lib/domain/utils.dart
@@ -1,5 +1,13 @@
+import 'package:meta/meta.dart';
+
 import 'map_point.dart';
 
-bool arePointsNear(MapPoint center, MapPoint point, double distance) {
-  return center.distanceTo(point) <= distance;
+bool arePointsNear({
+  @required MapPoint center,
+  @required MapPoint point,
+  @required double distanceMin,
+  @required double distanceMax,
+}) {
+  final distance = center.distanceTo(point);
+  return distance >= distanceMin && distance <= distanceMax;
 }
